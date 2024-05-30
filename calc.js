01416c57f5b6ba777cbd9c1657eba93b6dbf44e2
Ananya <patil08ananya@gmail.com>
2024-05-30 20:36:45 +0530

    calculator functions added

--- calc.js
+++ calc.js
@@ -1,7 +1,51 @@
-let calci_arr = [];
+let calciArr = [];
+let operator = ["+", "-", "*", "/", "%"];
+let result;
+let str;
+
+function updateScreen() {
+  document.getElementById("calci-screen").innerHTML = calciArr.join("");
+}
+
+function resetArr() {
+  calciArr = [];
+}
 
 function myFunction(num) {
-  console.log(num);
-  calci_arr.push(num);
-  console.log(calci_arr);
+  //document.getElementById("calci-screen").innerHTML = num;
+
+  //final output
+  if (num == "=") {
+    str = calciArr.join("");
+    try {
+      result = eval(str);
+      document.getElementById("calci-screen").innerHTML = result;
+    } catch (error) {
+      document.getElementById("error").innerHTML = error;
+    }
+    resetArr();
+  } else if (num == "AC") {
+    //All clear
+    resetArr();
+    document.getElementById("calci-screen").innerHTML = " ";
+  } else if (num == "back") {
+    //back
+    calciArr.pop();
+    updateScreen();
+  } else if (
+    operator.includes(num) &&
+    operator.includes(calciArr[calciArr.length - 1])
+  ) {
+    calciArr.pop();
+    calciArr.push(num);
+    updateScreen();
+  } else {
+    //numbers and operators
+    if (calciArr.length === 0 && operator.includes(num)) {
+      return;
+    } else {
+      calciArr.push(num);
+      updateScreen();
+    }
+  }
 }
